04aa091bfe00376b233b5c72fe35c05832692473
Resara <resarapamudithavcc@gmail.com>
2025-09-22 03:09:04 +0530

    chore: initial Spring Boot microservice

---
+++ myservice/src/test/java/com/resara/cicdproject/myservice/controller/HelloControllerTest.java
@@ -0,0 +1,15 @@
+package com.resara.cicdproject.myservice.controller;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloControllerTest {
+
+    private final HelloController controller = new HelloController();
+
+    @Test
+    void testSayHello() {
+        String response = controller.sayHello();
+        assertThat(response).isEqualTo("Hello from CI/CD pipeline project!");
+    }
+}
